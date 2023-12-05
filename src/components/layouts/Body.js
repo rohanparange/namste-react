1d5d1e4948c8b85b62399a81ebccc8e9558e124b
rohan <rohan.parange@neosoftmail.com>
2023-12-05 20:45:33 +0530

    shimmer scalaton and search functionality implemented

--- src/components/layouts/Body.js
+++ src/components/layouts/Body.js
@@ -1,28 +1,65 @@
 import RestaurantCard from "../RestaurantCard";
 import { RESTRO_DATA } from "../../utils/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = ()=>{
 
-    const [restroData,setRestroData] = useState(RESTRO_DATA);
+    const [restroData,setRestroData] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [filteredData,setFilteredData] = useState([]);
+    
+    useEffect(()=>{
+        fetchData();
+    },[])
 
-    return (
+    const fetchData = async ()=>{
+        try {
+            const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0177989&lng=72.84781199999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const jsonData = await data.json();
+            const restroData = jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            // console.log(jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+            // console.log(jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restruants)
+            setRestroData(RESTRO_DATA) ;
+            setFilteredData(RESTRO_DATA)
+            // setRestroData(restroData) ;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return filteredData.length === 0 ? <Shimmer/> : (
         <div className="body">
-            {/* <div className="search">Search</div> */}
+            <div className="sub-header">
+            <div className="search">
+                <input type="text" className="" value={searchText} onChange={(e)=>{
+                    setSearchText(e.target.value)
+                }}/>
+            </div>
+            <button className="filter_btn"
+            onClick={()=>{
+                const searchedRestro =  restroData.filter(data=>{
+                    return data.info.name.toLowerCase().includes(searchText.toLowerCase())
+                })
+
+                setFilteredData(searchedRestro)
+            }}>Search</button>
             <button 
                 className="filter_btn"
                 onClick={()=>{
                     const filterData = restroData.filter(data=>{
                         return data.info.avgRating>4.5
                     })
-                    setRestroData(filterData)
+                    setFilteredData(filterData)
                 }}>
                     Filter Data
             </button>
+            </div>
+            
             <div className="res-container">
 
                 {
-                    restroData.map((data)=>(
+                    filteredData.map((data)=>(
                         <RestaurantCard key={data.info.id} data={data}/>
                     ))
                 }
---
+++ src/components/layouts/Shimmer.js
@@ -0,0 +1,61 @@
+const Shimmer =()=>{
+    return (
+        <duv className="shimmer-container">
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div><div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div><div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div><div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div><div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div><div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            <div className="shimmer-card">
+                <div className="shimmer-logo"></div>
+            </div>
+            
+        </duv>
+    )
+}
+
+export default Shimmer;
